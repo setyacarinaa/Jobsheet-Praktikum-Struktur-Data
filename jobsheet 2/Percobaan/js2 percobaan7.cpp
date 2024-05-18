7e0c9b3042a28af32541bc0432780f9e3c652183
tyaa <145129875+setyacarinaa@users.noreply.github.com>
2024-05-18 22:20:10 +0700

    Jobsheet 2

---
+++ jobsheet 2/Percobaan/js2 percobaan7.cpp
@@ -0,0 +1,22 @@
+// setya carina rianti 23343054
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Mahasiswa {
+	char nama [50];
+	char jurusan [50]; 
+	int nilai;
+}data;
+
+int main(void)
+{
+	data mhs01;
+	strcpy(mhs01.nama, "Setya Carina Rianti");
+	strcpy(mhs01.jurusan, "Informatika");
+	mhs01.nilai = 100;
+	
+	printf("%s adalah mahasiswa jurusan %s ", mhs01.nama, mhs01.jurusan);
+	printf("memperoleh nilai UAS %i \n", mhs01.nilai);
+	return 0;
+}
---
+++ jobsheet 2/Percobaan/js2 percobaan8.cpp
@@ -0,0 +1,22 @@
+// setya carina rianti 23343054
+
+#include <stdio.h>
+#include <conio.h>
+
+typedef int angka;
+typedef char huruf;
+
+int main(){
+	angka umur;
+	huruf h;
+	huruf nama[10];
+	
+	printf("Masukkan umur anda : ");scanf("%d",&umur);
+	printf("Umur anda adalah %d",umur);
+	printf("\nMasukkan huruf : "); h=getche();
+	printf("\nHuruf anda %c",h);
+	printf("\nMasukkan nama : ");scanf("%s",nama);
+	printf("Nama anda %s",nama);
+	
+	getch();
+}
