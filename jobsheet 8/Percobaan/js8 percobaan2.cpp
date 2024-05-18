9d6cf043d8451bb61e1c85c5fb9f03c4ba47d51a
tyaa <145129875+setyacarinaa@users.noreply.github.com>
2024-05-18 21:54:46 +0700

    Jobsheet 8

---
+++ jobsheet 8/Percobaan/js8 percobaan2.cpp
@@ -0,0 +1,25 @@
+//Setya Carina Rianti 23343054
+
+#include<stdio.h>
+
+int main() {
+    int i, j, temp;
+    int a[10] = { 10, 9, 7, 101, 23, 44, 12, 78, 34, 23 };
+
+    for (i = 0; i < 10; i++) {
+        for (j = i + 1; j < 10; j++) {
+            if (a[j] < a[i]) { 
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("Printing Sorted Element List in Ascending Order...\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
+}
