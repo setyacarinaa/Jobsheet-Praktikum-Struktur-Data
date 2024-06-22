0eec3114ff7fa244c2e6b6264783d8944083e74c
tyaa <145129875+setyacarinaa@users.noreply.github.com>
2024-06-22 18:34:39 +0700

    Delete jobsheet 1 directory

--- jobsheet 1/js1 percobaan1.cpp
+++
@@ -1,8 +0,0 @@
-// setya carina rianti 23343054
-
-#include <stdio.h> 
-int main()
-{
-	printf("\n Welcome to the world of C ");// prints the message on the screen 
-	return 0;// returns a value 0 to the operating system
-}
